0ca338ae2d5dfa9112f43003594fa639f1db026e
gamerforEA <gamerforEA@gmail.com>
2016-12-15 16:55:46 +0400

    Fix config for Scrapbox in Dispenser.

---
+++ src/main/java/ic2/core/item/BehaviorScrapboxDispense.java
@@ -0,0 +1,28 @@
+package ic2.core.item;
+
+import com.gamerforea.ic2.EventConfig;
+
+import ic2.api.recipe.Recipes;
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+
+public class BehaviorScrapboxDispense extends BehaviorDefaultDispenseItem
+{
+	@Override
+	protected ItemStack dispenseStack(IBlockSource blockSource, ItemStack stack)
+	{
+		// TODO gamerforEA code start
+		if (!EventConfig.scrapboxDropEnabled)
+			return stack;
+		// TODO gamerforEA code end
+
+		EnumFacing facing = EnumFacing.getFront(blockSource.getBlockMetadata());
+		IPosition position = BlockDispenser.func_149939_a(blockSource);
+		doDispense(blockSource.getWorld(), Recipes.scrapboxDrops.getDrop(stack, true), 6, facing, position);
+		return stack;
+	}
+}
